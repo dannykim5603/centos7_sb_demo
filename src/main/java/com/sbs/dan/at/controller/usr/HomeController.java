b50c270d72054602077ffbbc08dea8e5d9aa1f18
dannykim <dannykim5603@gmail.com>
2020-09-14 22:57:16 +0900

    sessions

--- src/main/java/com/sbs/dan/at/controller/usr/HomeController.java
+++ src/main/java/com/sbs/dan/at/controller/usr/HomeController.java
@@ -1,5 +1,7 @@
 package com.sbs.dan.at.controller.usr;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,20 @@ public class HomeController {
 	@RequestMapping("/")
 	@ResponseBody
 	public String showMain() {
-		return articleDao.getAllCounnt() + "개";
+		return articleDao.getAllCounnt() + "개가 있음~!!";
+	}
+
+	@RequestMapping("/doSetSession")
+	@ResponseBody
+	public String doSetSession(HttpSession session) {
+		session.setAttribute("a",10);
+		return "세션 설정 완료";
+	}
+	
+	@RequestMapping("/getSession")
+	@ResponseBody
+	public int getSession(HttpSession session) {
+		return (int)session.getAttribute("a");
 	}
 }
+
